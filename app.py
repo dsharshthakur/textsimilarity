4cf72da2dd093865228501efe4acc7659b6dda39
HARSH SINGH <131766995+dsharshthakur@users.noreply.github.com>
2024-04-13 23:29:21 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from Helping import TextSimilarity, model
+
+# main title
+st.title("Text Similarity Check")
+st.markdown("<br>", unsafe_allow_html=True)
+
+# text inputs
+text1 = st.text_input("Enter first text:")
+text2 = st.text_input("Enter second text:")
+
+st.markdown("<br>", unsafe_allow_html=True)
+
+# calculate button
+col1, col2, col3 = st.columns(3)
+with col2:
+    btn = st.button("Calculate")
+
+# perform calculation and display output
+if btn:
+    score = TextSimilarity(text1, text2, model)
+    similarity = round(score.calculate(), 2)
+    col1, col2, col3 = st.columns(3)
+
+    # if similarity score is negative
+    if similarity < 0:
+        similarity = 0
+
+    # display final output
+    st.success(similarity)
